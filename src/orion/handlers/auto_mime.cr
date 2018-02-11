aa0e013eac14edebc574c63b20aa4d2dae88dcd7
jwaldrip <jason@waldrip.net>
2018-02-10 19:18:03 -0700

    fix existing specs

--- src/orion/handlers/auto_mime.cr
+++ src/orion/handlers/auto_mime.cr
@@ -3,13 +3,15 @@ class Orion::Handlers::AutoMime
   include HTTP::Handler
 
   def call(cxt : HTTP::Server::Context)
-    cxt.request.headers["Accept"] ||= type_from_path?(cxt.request)
+    if !cxt.request.headers["Accept"]? && (mime_type = type_from_path?(cxt.request))
+      cxt.request.headers["Accept"] = mime_type
+    end
     call_next cxt
   end
 
   private def type_from_path?(req : HTTP::Request)
-    return if File.extname(cxt.request.path).empty?
-    mime_type = MIME::Types.type_for(cxt.request.path).first?
+    return if File.extname(req.path).empty?
+    mime_type = MIME::Types.type_for(req.path).first?
     mime_type.to_s if mime_type
   end
 end
--- src/orion/handlers/method_override_param.cr
+++ src/orion/handlers/method_override_param.cr
@@ -12,11 +12,11 @@ class Orion::Handlers::MethodOverrideParam
   end
 
   private def param_method?(req : HTTP::Request)
-    cxt.request.params["_method"]?
+    req.query_params["_method"]?
   end
 
   private def form_method?(req : HTTP::Request)
-    if MIME::Types.type_for(cxt.request).includes? MIME::Types["multipart/form-data"]
+    if MIME::Types.type_for(req).first? == MIME::Types["multipart/form-data"]
       HTTP::FormData.parse(req) do |part|
         if part.name == "_method"
           return part.body.gets_to_end
