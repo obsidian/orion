4e4561ea16686838aebad4e0c4656ca6362dd7a6
jwaldrip <jason@waldrip.net>
2018-02-10 22:19:39 -0700

    add specs

---
+++ spec/orion/handlers/method_override_param_spec.cr
@@ -0,0 +1,24 @@
+require "../../spec_helper"
+
+describe Orion::Handlers::MethodOverrideParam do
+  context "given a query param" do
+    it "should set override the method" do
+      context = mock_context(:get, "/?_method=POST")
+      Orion::Handlers::MethodOverrideParam.new.call(context)
+      context.request.method.should eq "POST"
+    end
+  end
+
+  context "given a form param" do
+    it "should set override the method" do
+      io = IO::Memory.new
+      builder = HTTP::FormData::Builder.new(io)
+      builder.field("_method", "POST")
+      builder.finish
+      io.rewind
+      context = mock_context(:get, "/", body: io, headers: { "Content-Type" => "multipart/form-data; boundary=\"#{builder.boundary}\"" })
+      Orion::Handlers::MethodOverrideParam.new.call(context)
+      context.request.method.should eq "POST"
+    end
+  end
+end
