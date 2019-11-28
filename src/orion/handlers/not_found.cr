c7c719128fc517afd433c87a2370dbb1fa84d57a
jwaldrip <jason@waldrip.net>
2019-11-28 00:36:31 -0700

    simplify handler

--- src/orion/handlers/not_found.cr
+++ src/orion/handlers/not_found.cr
@@ -2,9 +2,6 @@ class Orion::Handlers::NotFound
   include HTTP::Handler
 
   def call(cxt : HTTP::Server::Context)
-    cxt.response.respond_with_error(
-      message: HTTP::Status.new(404).description,
-      code: 404
-    )
+    cxt.response.respond_with_status(:not_found)
   end
 end
