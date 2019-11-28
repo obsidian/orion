85e9059672d5bffa25ca9897f95771a0ceb48329
jwaldrip <jason@waldrip.net>
2019-11-28 00:35:48 -0700

    define all possible definitions

--- spec/orion/router_spec.cr
+++ spec/orion/router_spec.cr
@@ -3,10 +3,93 @@ require "../spec_helper"
 params = {} of String => String
 
 module RouterSpec
+  class SampleController
+    include Orion::ControllerHelper
+
+    def action
+      params = request.path_params
+    end
+  end
+
+  class WSSampleController
+    include Orion::WebSocketControllerHelper
+
+    def action
+      ws.send "hello world"
+    end
+  end
+
   router Router do
     root ->(c : Context) { c.response.print "I am Groot" }
+    root to: "SampleController#action"
+    root controller: SampleController, action: action
+    root do |c|
+      params = c.request.path_params
+    end
     get "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    get "/:first/:second", to: "SampleController#action"
+    get "/:first/:second", controller: SampleController, action: action
+    get "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    head "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    head "/:first/:second", to: "SampleController#action"
+    head "/:first/:second", controller: SampleController, action: action
+    head "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    post "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    post "/:first/:second", to: "SampleController#action"
+    post "/:first/:second", controller: SampleController, action: action
+    post "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    put "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    put "/:first/:second", to: "SampleController#action"
+    put "/:first/:second", controller: SampleController, action: action
+    put "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    delete "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    delete "/:first/:second", to: "SampleController#action"
+    delete "/:first/:second", controller: SampleController, action: action
+    delete "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    connect "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    connect "/:first/:second", to: "SampleController#action"
+    connect "/:first/:second", controller: SampleController, action: action
+    connect "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    options "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    options "/:first/:second", to: "SampleController#action"
+    options "/:first/:second", controller: SampleController, action: action
+    options "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    trace "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    trace "/:first/:second", to: "SampleController#action"
+    trace "/:first/:second", controller: SampleController, action: action
+    trace "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    patch "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    patch "/:first/:second", to: "SampleController#action"
+    patch "/:first/:second", controller: SampleController, action: action
+    patch "/:first/:second" do |c|
+      params = c.request.path_params
+    end
+    match "/:first/:second", ->(c : Context) { params = c.request.path_params }
+    match "/:first/:second", to: "SampleController#action"
+    match "/:first/:second", controller: SampleController, action: action
+    match "/:first/:second" do |c|
+      params = c.request.path_params
+    end
     put "/hello", ->(c : Context) { c.response.print "I put things" }
+    ws "/socket", ->(s : WebSocket, c : Context) {
+      s.send "hello world"
+    }
   end
 
   describe "a basic router" do
