b3558f558b10b7eeaa8f7edebad4634a96e23df8
jwaldrip <jason@waldrip.net>
2020-02-04 21:10:02 -0700

    breaking change to form name#action

--- spec/orion/router_spec.cr
+++ spec/orion/router_spec.cr
@@ -7,78 +7,78 @@ module RouterSpec
     static path: "/assets", dir: "./spec/fixtures"
     root ->(c : Context) { c.response.print "I am Groot" }
     root to: "sample#action"
-    root to: "SampleController#action"
+    root to: "Sample#action"
     root controller: SampleController, action: action
     root do |c|
       params = c.request.path_params
     end
     get "/:first/:second", ->(c : Context) { params = c.request.path_params }
     get "/:first/:second", to: "sample#action"
-    get "/:first/:second", to: "SampleController#action"
-    get "/:first/:second", to: "SampleController#action"
+    get "/:first/:second", to: "Sample#action"
+    get "/:first/:second", to: "Sample#action"
     get "/:first/:second", controller: SampleController, action: action
     get "/:first/:second" do |c|
       params = c.request.path_params
     end
     head "/:first/:second", ->(c : Context) { params = c.request.path_params }
     head "/:first/:second", to: "sample#action"
-    head "/:first/:second", to: "SampleController#action"
+    head "/:first/:second", to: "Sample#action"
     head "/:first/:second", controller: SampleController, action: action
     head "/:first/:second" do |c|
       params = c.request.path_params
     end
     post "/:first/:second", ->(c : Context) { params = c.request.path_params }
     post "/:first/:second", to: "sample#action"
-    post "/:first/:second", to: "SampleController#action"
+    post "/:first/:second", to: "Sample#action"
     post "/:first/:second", controller: SampleController, action: action
     post "/:first/:second" do |c|
       params = c.request.path_params
     end
     put "/:first/:second", ->(c : Context) { params = c.request.path_params }
     put "/:first/:second", to: "sample#action"
-    put "/:first/:second", to: "SampleController#action"
+    put "/:first/:second", to: "Sample#action"
     put "/:first/:second", controller: SampleController, action: action
     put "/:first/:second" do |c|
       params = c.request.path_params
     end
     delete "/:first/:second", ->(c : Context) { params = c.request.path_params }
     delete "/:first/:second", to: "sample#action"
-    delete "/:first/:second", to: "SampleController#action"
+    delete "/:first/:second", to: "Sample#action"
     delete "/:first/:second", controller: SampleController, action: action
     delete "/:first/:second" do |c|
       params = c.request.path_params
     end
     connect "/:first/:second", ->(c : Context) { params = c.request.path_params }
     connect "/:first/:second", to: "sample#action"
-    connect "/:first/:second", to: "SampleController#action"
+    connect "/:first/:second", to: "Sample#action"
     connect "/:first/:second", controller: SampleController, action: action
     connect "/:first/:second" do |c|
       params = c.request.path_params
     end
     options "/:first/:second", ->(c : Context) { params = c.request.path_params }
     options "/:first/:second", to: "sample#action"
-    options "/:first/:second", to: "SampleController#action"
+    options "/:first/:second", to: "Sample#action"
     options "/:first/:second", controller: SampleController, action: action
     options "/:first/:second" do |c|
       params = c.request.path_params
     end
     trace "/:first/:second", ->(c : Context) { params = c.request.path_params }
     trace "/:first/:second", to: "sample#action"
-    trace "/:first/:second", to: "SampleController#action"
+    trace "/:first/:second", to: "Sample#action"
     trace "/:first/:second", controller: SampleController, action: action
     trace "/:first/:second" do |c|
       params = c.request.path_params
     end
     patch "/:first/:second", ->(c : Context) { params = c.request.path_params }
     patch "/:first/:second", to: "sample#action"
-    patch "/:first/:second", to: "SampleController#action"
+    patch "/:first/:second", to: "Sample#action"
     patch "/:first/:second", controller: SampleController, action: action
     patch "/:first/:second" do |c|
       params = c.request.path_params
     end
     match "/:first/:second", ->(c : Context) { params = c.request.path_params }
     match "/:first/:second", to: "sample#action"
-    match "/:first/:second", to: "SampleController#action"
+    match "/:first/:second", to: "Sample#action"
     match "/:first/:second", controller: SampleController, action: action
     match "/:first/:second" do |c|
       params = c.request.path_params
@@ -88,7 +88,7 @@ module RouterSpec
       s.send "hello world"
     }
     ws "/:first/:second", to: "web_socket_sample#action"
-    ws "/:first/:second", to: "WebSocketSampleController#action"
+    ws "/:first/:second", to: "WebSocketSample#action"
     ws "/:first/:second", controller: WebSocketSampleController, action: action
     ws "/:first/:second" do |s, c|
       s.send "hello world"
--- src/orion/router/inflector/controllerize.cr
+++ src/orion/router/inflector/controllerize.cr
@@ -1,7 +1,7 @@
 require "inflector"
 
 if ARGV[0][0].ascii_uppercase?
-  print ARGV[0]
+  print ARGV[0] + "Controller"
 else
   print Inflector.camelize(ARGV[0]) + "Controller"
 end
