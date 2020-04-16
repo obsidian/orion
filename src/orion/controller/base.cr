2c75017902f532ae0361c9e428817a156535fe5b
jwaldrip <jason@waldrip.net>
2020-04-16 12:00:33 -0600

    update base controller

--- src/orion/controller/base.cr
+++ src/orion/controller/base.cr
@@ -1,20 +1,38 @@
 abstract class Orion::Controller::Base
+  DEFAULT_LAYOUT = ""
+
   getter context : ::HTTP::Server::Context
   delegate request, response, to: @context
+
+  macro inherited
+    layout "application.ecr"
+  end
 
   macro set_template_path(path)
     {% TEMPLATE_PATH = path %}
   end
 
-  macro render(template, *, status = 200, content_type = nil)
-    response.status = status
-    Kilt.embed {{ template }}
+  macro layout(filename)
+    DEFAULT_LAYOUT = {{ filename }}
+  end
+
+  macro render(*, layout = DEFAULT_LAYOUT)
+    __kilt_io__ = response
+    Kilt.embed "layouts/{{ layout }}"
+  end
+
+  macro render(*, json)
+    {{ json }}.to_json(response)
+  end
+
+  macro render(*, yaml)
+    {{ yaml }}.to_yaml(response)
+  end
+
+  macro render(*, text)
+    response.puts({{ text }})
   end
 
   def initialize(@context)
-  end
-
-  private def __kilt_io__
-    response
   end
 end
