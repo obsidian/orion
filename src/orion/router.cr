16847aaa18363b5289392f3bfc129b485955dac7
jwaldrip <jason@waldrip.net>
2020-04-16 11:59:56 -0600

    update bind options

--- src/orion/router.cr
+++ src/orion/router.cr
@@ -1,5 +1,4 @@
 abstract class Orion::Router; end
-
 require "./router/*"
 
 abstract class Orion::Router
@@ -23,15 +22,9 @@ abstract class Orion::Router
   # :nodoc:
   CONTROLLER = nil
 
-  @app : HTTP::Handler
+  @stack : HTTP::Handler
 
-  delegate call, to: @app
-
-  def self.listen(*, autoclose = true, host = "127.0.0.1", port = 3000, reuse_port = false)
-    router = new(autoclose: autoclose)
-    server = HTTP::Server.new(handler: router)
-    server.listen(host: host, port: port, reuse_port: reuse_port)
-  end
+  delegate call, to: @stack
 
   # :nodoc:
   def self.normalize_path(path : String)
@@ -65,6 +58,13 @@ abstract class Orion::Router
     tree.visualize
   end
 
+  def self.start(autoclose : Bool = true, workers = nil, **bind_opts)
+    new(autoclose).tap do |server|
+      server.bind(**bind_opts)
+      workers ? server.listen(workers) : server.listen
+    end
+  end
+
   def initialize(autoclose : Bool = true)
     use Handlers::AutoClose if autoclose
     use Handlers::MethodOverrideHeader
@@ -72,6 +72,60 @@ abstract class Orion::Router
     use Handlers::Meta
     use Handlers::RouteFinder.new(self.class.tree)
     use Handlers::NotFound
-    @app = HTTP::Server.build_middleware handlers
+    @stack = HTTP::Server.build_middleware handlers
+    @server = HTTP::Server.new(handler: @stack)
+  end
+
+
+  # Bind to a socket using TLS
+  def bind(*, tls, **bind_opts)
+    @server.bind_tls(**bind_opts, context: tls)
+  end
+
+  # Bind to a port
+  def bind(*, port, **bind_opts)
+    @server.bind_tcp(**bind_opts, port: port)
+  end
+
+  # Bind to a Socket::IPAddress
+  def bind(*, address, **bind_opts)
+    @server.bind_tcp(**bind_opts, address: address)
+  end
+
+  # Bind to a Socket::UnixAddress
+  def bind(*, path, **bind_opts)
+    @server.bind_unix(**bind_opts, path: path)
+  end
+
+  # Bind to a random, unused Socket::IPAddress
+  def bind(*args, **bind_opts)
+    @server.bind_unused_port(**bind_opts)
+  end
+
+  # Listen clients using multiple workers
+  # A good suggestion is to use System.cpu_count
+  def listen(*, workers, **opts)
+    workers.times do |i|
+      Process.fork do
+        listen(**opts, reuse_port: true, prefix: "#{self.class.name}.#{i}")
+      end
+    end
+    sleep
+  end
+
+  # Listen for clients
+  def listen(*args, prefix = self.class.name, **opts)
+    @server.each_address do |address|
+      listen_message(address, prefix)
+    end
+    @server.listen
+  end
+
+  private def listen_message(socket : ::Socket::UNIXAddress, prefix)
+    puts "#{prefix}: listening on #{socket.path}"
+  end
+
+  private def listen_message(socket : ::Socket::IPAddress, prefix)
+    puts "#{prefix}: listening on #{socket.address}:#{socket.port}"
   end
 end
