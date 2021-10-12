86cfdc204bbf3d654d68f17d69b4a5da136500dc
jwaldrip <jason@waldrip.net>
2021-10-12 14:11:34 -0600

    fix autoclose error

--- src/orion/handlers/auto_close.cr
+++ src/orion/handlers/auto_close.cr
@@ -5,5 +5,7 @@ class Orion::Handlers::AutoClose
   def call(cxt : Server::Context)
     call_next cxt
     cxt.response.close unless cxt.response.closed?
+  rescue
+    # everything is fine, dont raise an error
   end
 end
